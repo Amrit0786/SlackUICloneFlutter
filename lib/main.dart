676095778a3bd19e5bc289b90c6d37f2aa5fdbcd
amrit.mishra <amrit.mishra@byjus.com>
2020-09-05 14:29:22 +0530

    1. Made status bar color exactly as app bar color using method SystemUiOverlayStyle. 2. Also made scaffold and app bar part of main.dart. 3. Made similar search bar like slack using container.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:slack_clone_app_ui/config/palette.dart';
 import 'package:slack_clone_app_ui/screens/screens.dart';
 
 void main() {
+  /* Makes statusBar color same as appBar color.
+  It is different from setting up the brightness
+  in which status bar color will not be exactly
+  as you appBar color.
+  */
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Palette.appbar,
+    ),
+  );
   runApp(MyApp());
 }
 
@@ -12,7 +24,35 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Slack ',
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(45),
+          child: AppBar(
+            backgroundColor: Palette.appbar,
+            title: Text(
+              'Team11',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.1),
+            ),
+            actions: [
+              IconButton(
+                padding: EdgeInsets.only(right: 10),
+                icon: Icon(
+                  FlutterIcons.search1_ant,
+                  size: 20,
+                  color: Colors.white,
+                ),
+                onPressed: () => print('search'),
+              )
+            ],
+            elevation: 0,
+          ),
+        ),
+        body: HomeScreen(),
+      ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,34 +5,39 @@ import 'package:slack_clone_app_ui/config/palette.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(45),
-          child: AppBar(
-            backgroundColor: Palette.appbar,
-            title: Text(
-              'Team11',
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: -1.1),
-            ),
-            actions: [
-              IconButton(
-                padding: EdgeInsets.only(right: 10),
-                icon: Icon(
-                  FlutterIcons.search1_ant,
-                  size: 20,
-                  color: Colors.white,
+    return Scrollbar(
+      child: Column(
+        children: [
+          Container(
+            height: 40,
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.all(15),
+            padding: EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(
+                  width: 0.3,
+                  color: Colors.black38,
                 ),
-                onPressed: () => print('search'),
-              )
-            ],
-            elevation: 0,
-          ),
-        ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black38,
+                    offset: Offset(-0.5, 0),
+                    blurRadius: 2,
+                  )
+                ]),
+            child: Text(
+              'Jump to...',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
