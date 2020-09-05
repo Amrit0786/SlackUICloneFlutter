73e5299807c8fce6771693175cb13a4c72693c97
amrit.mishra <amrit.mishra@byjus.com>
2020-09-05 17:59:50 +0530

    1. Showing inkwell highlight color without splash on tapping on the search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
-import 'package:slack_clone_app_ui/config/palette.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -11,29 +9,52 @@ class HomeScreen extends StatelessWidget {
           Container(
             height: 40,
             width: MediaQuery.of(context).size.width,
-            alignment: Alignment.centerLeft,
             margin: EdgeInsets.all(15),
-            padding: EdgeInsets.only(left: 10),
             decoration: BoxDecoration(
                 shape: BoxShape.rectangle,
-                color: Colors.white,
                 borderRadius: BorderRadius.circular(5),
                 border: Border.all(
-                  width: 0.3,
+                  width: 0.2,
                   color: Colors.black38,
                 ),
                 boxShadow: [
                   BoxShadow(
                     color: Colors.black38,
-                    offset: Offset(-0.5, 0),
-                    blurRadius: 2,
+                    blurRadius: 0.5,
                   )
                 ]),
-            child: Text(
-              'Jump to...',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black54,
+            child: ClipRRect(
+              //ClipRRect because otherwise on tap of ink well ink goes out of bound.
+              borderRadius: BorderRadius.circular(5),
+              child: Material(
+                // It is used with white color so that searchBar get background color as white and we see the tap effect with inkWell
+                color: Colors.white,
+                child: InkWell(
+                  /*Colors.transparent is used to basically disable splash effect. 
+                  To make exactly like Slack animation we can create our own CustomClassFactory 
+                  with varied speed of animation and size of splash.
+                   */
+                  splashColor: Colors.transparent,
+                  onTap: () {
+                    print('search bar');
+                  },
+                  /*Previous container was used to make search bar appear little elevated. 
+                  Inkwell highlight effect could not have been seen with the shadow with parent container.
+                   */
+                  child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      'Jump to...',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ),
           )
